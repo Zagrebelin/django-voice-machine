b43c2a29df064044ca5ab83b43fa7eb1ee1a32dd
zagrebelin <pavel@zagrebelin.ru>
2017-04-24 16:37:06 +0500

    prepare management command готовится га одну минуту вперёд

--- apps/voice/management/commands/prepare.py
+++ apps/voice/management/commands/prepare.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime, timedelta
 
 from ...models import ScheduleItem
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         from ... import tools
-        items = ScheduleItem.objects.for_date().order_by('order').all()
+        dt = datetime.now() + timedelta(minutes=1)
+        items = ScheduleItem.objects.for_date(dt).order_by('order').all()
         tools.download(items)
