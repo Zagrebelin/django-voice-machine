4bcb40c9a3b5f2b7ad0abdea49099c9c55adc808
Pavel Zagrebelin <pavel@zagrebelin.ru>
2018-11-10 12:35:35 +0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-voice-machine',
-    version='1.3.1',
+    version='1.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['django', 'requests', 'lxml'],
